58cb74c755927f232635e465a49ce7eb457ad383
Carsten Brandt <cbrandt@metadesign.de>
2011-03-08 20:38:03 +0100

    adjusted config

--- app/config/console.php
+++ app/config/console.php
@@ -6,9 +6,16 @@ defined('DS') OR define('DS', DIRECTORY_SEPARATOR);
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(dirname(__FILE__)),
-	'name'=>'My Console Application',
+	'name'=>'Yii-App-Test-Runner',
 	// application components
+	'commandMap' => array(
+		'tests'=>array(
+			'class'=>'application.commands.testRunner.TestrunnerCommand',
+		),
+	),
+
 	'components'=>array(
+
 		'db'=>array(
 			'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
 		),
